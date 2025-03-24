69078ace4aad17d95fe086ca67b9710206fa8aa5
SurrealMoon <wwebsterr93@gmail.com>
2025-03-24 11:31:14 +0300

    Frontend setup

---
+++ zivo/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <View className="w-10 h-10 bg-red-600" />
+      <Text className="text-9xl color-red-700">Home Screen</Text>
+    </View>
+  );
+}
