cc8d6d1e13ff8f743f7471d3e13dc35c02766d1d
SurrealMoon <wwebsterr93@gmail.com>
2025-05-18 16:13:52 +0300

    fix: login routes

--- zivo/app/_layout.tsx
+++ zivo/app/_layout.tsx
@@ -2,45 +2,34 @@ import '@/i18n/i18n';
 import React, { useEffect, useState } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { Slot, useRouter } from 'expo-router';
+import { Slot } from 'expo-router';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { StatusBar } from 'expo-status-bar';
 import { ZivoQueryProvider } from '@/context/QueryClientProvider';
 import Toast from 'react-native-toast-message';
 import { I18nManager } from 'react-native';
+
 import * as Updates from 'expo-updates';
-import { getMe } from '@/services/auth.service'; // 🔸 getMe fonksiyonunu kullandığını varsayıyorum
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loading, setLoading] = useState(true);
+
   const [fontsLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  const router = useRouter();
-
   useEffect(() => {
     const prepareApp = async () => {
+      // RTL'i tamamen kapat (hem izin hem yön)
       if (I18nManager.isRTL) {
         I18nManager.allowRTL(false);
         I18nManager.forceRTL(false);
+
+    
         await Updates.reloadAsync();
         return;
-      }
-
-      try {
-        // ✅ Kullanıcı login mi kontrol et
-        const user = await getMe();
-        if (user) {
-          router.replace('/(user)/(tabs)');
-        } else {
-router.replace('/(auth)/login');
-        }
-      } catch (error) {
-        // Token yoksa veya geçersizse login'e yönlendir
-router.replace('/(auth)/login');
       }
 
       if (fontsLoaded) {
@@ -52,7 +41,9 @@ router.replace('/(auth)/login');
     prepareApp();
   }, [fontsLoaded]);
 
-  if (loading) return null;
+  if (loading) {
+    return null;
+  }
 
   return (
     <ZivoQueryProvider>
